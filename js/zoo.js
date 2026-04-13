8934b09ba5635ffc30b11da2f1555ffb2fcfbb92
dev-medina29 <mmbedi@byupathway.edu>
2026-04-13 23:50:12 +0100

    save ccs2

--- js/zoo.js
+++ js/zoo.js
@@ -1,14 +1,3 @@
-const date = new Date();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const day = date.getDate();
-current_year = document.querySelector("#currentyear");
-current_year.innerHTML = `${year}`;
-current_date = document.querySelector("#lastModified");
-current_date.innerHTML = `Last modification ${month}/${day}/${year} at ${hours}:${minutes}:${seconds}`;
 coordinate = {
   lat: -4.271667229260911,
   lng: 15.260030653062937,
